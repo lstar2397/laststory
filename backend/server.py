a5f06cf9852c60d0ef1f96c998433f61711d2fb2
dora2emong <zhj7634@naver.com>
2023-05-24 22:25:13 +0900

    로그인

---
+++ backend/server.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+from pymongo import MongoClient
+from configparser import ConfigParser
+
+# Config
+config = ConfigParser()
+config.read('config.ini')
+
+# DB connection
+client = MongoClient(config['DATABASE']['ADDRESS'])
+
+db = client['test-db']
+
+app = Flask(__name__)
+
+
+@app.route('/sign_up', methods=['POST'])
+def sign_up():
+    data = request.get_json(cache=False)
+    username = data['username']
+    password = data['password']
+    nickname = data['nickname']
+    email = data['email']
+    print(username, password, nickname, email)
+    user = db.user.find_one({'username': username})
+
+    if user is not None:
+        return {'result': 'fail', 'message': 'username already exists'}, 409
+    else:
+        user_info = {
+            'username': username, 
+            'password': password, 
+            'nickname': nickname, 
+            'email': email
+            }
+        db.posts.insert_one(user_info)
+        return {'result': 'success'}, 200
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
+
