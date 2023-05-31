27eddd866999fac7d8351c215fd79c921f4a5556
Euijin Song <lstar2397@naver.com>
2023-05-31 18:57:30 +0900

    Update contract communication

--- blockchain/letterexchange.sol
+++ blockchain/letterexchange.sol
@@ -1,91 +1,30 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
 
 contract LetterExchange {
     struct Letter {
-        string contentIpfsUrl;
         address sender;
         address receiver;
-        uint publicationDate;
+        string contentIdentifier;
     }
 
-    Letter[] internal letters;
-    mapping (uint => address) letterToOwner;
+    Letter[] public letters;
+    mapping (uint256 => address) letterToOwner;
 
-    function createLetter(string memory _contentIpfsUrl, address _receiver, uint _publicationDate) public {
-        letters.push(Letter({
-            contentIpfsUrl: _contentIpfsUrl,
-            sender: msg.sender,
-            receiver: _receiver,
-            publicationDate: _publicationDate
-        }));
-
-        uint letterId = letters.length - 1;
+    function send(address _receiver, string memory _contentIdentifier) external returns (uint256) {
+        Letter memory letter = Letter(msg.sender, _receiver, _contentIdentifier);
+        letters.push(letter);
+        uint256 letterId = letters.length - 1;
         letterToOwner[letterId] = msg.sender;
+
+        return letterId;
     }
 
-    function getLetter(uint _letterId) external view returns (string memory, address, address) {
+    function get(uint256 _letterId) public returns (address, address, string memory) {
+        require(_letterId < letters.length);
+        require(letterToOwner[_letterId] == msg.sender);
+
         Letter memory letter = letters[_letterId];
-
-        require (letter.sender == msg.sender || 
-                letterToOwner[_letterId] == msg.sender);
-
-        return (letter.contentIpfsUrl,
-                letter.sender,
-                letter.receiver);
-    }
-
-    function _getCountBySender() internal view returns (uint) {
-        uint count = 0;
-
-        for (uint i = 0; i < letters.length; i++) {
-            if (letters[i].sender == msg.sender) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    function getLettersBySender() external view returns (uint[] memory) {
-        uint[] memory result = new uint[](_getCountBySender());
-
-        uint j = 0;
-        for (uint i = 0; i < letters.length; i++) {
-            if (letters[i].sender == msg.sender) {
-                result[j] = i;
-                j++;
-            }
-        }
-
-        return result;
-    }
-
-    function _getCountByReceiver() internal view returns (uint) {
-        uint count = 0;
-
-        for (uint i = 0; i < letters.length; i++) {
-            if (letters[i].receiver == msg.sender &&
-                letters[i].publicationDate >= block.timestamp) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    function getLettersByReceiver() external view returns (uint[] memory) {
-        uint[] memory result = new uint[](_getCountByReceiver());
-
-        uint j = 0;
-        for (uint i = 0; i < letters.length; i++) {
-            if (letters[i].receiver == msg.sender &&
-                letters[i].publicationDate >= block.timestamp) {
-                result[j] = i;
-                j++;
-            }
-        }
-
-        return result;
+        return (letter.sender, letter.receiver, letter.contentIdentifier);
     }
 }
