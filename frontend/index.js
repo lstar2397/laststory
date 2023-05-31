79df6d4f28df218e10a3b4b7eb8c7a10318cd95b
Euijin Song <lstar2397@naver.com>
2023-05-31 19:27:38 +0900

    Remove unnecessary code

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const axios = require("axios");
 const bodyParser = require("body-parser");
-const e = require("express");
 const app = express();
 
 const PORT = 3000 || process.env.PORT;
