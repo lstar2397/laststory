52d073214a70faacad9a489f881cc6e50f5ee530
dora2emong <44566304+dora2emong@users.noreply.github.com>
2023-06-07 23:06:37 +0900

    feat: jwt data 전송

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify, session
 from pymongo import MongoClient
 from configparser import ConfigParser
 from email.message import EmailMessage
+from flask_jwt_extended import JWTManager, create_access_token
 
 # Config
 config = ConfigParser()
@@ -10,37 +11,42 @@ config.read('config.ini')
 
 # DB connection
 client = MongoClient(config['DATABASE']['ADDRESS'])
-
 db = client['test-db']
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
-app.secret_key = config['SECRET_KEY']['KEY']
+app.config['JWT_SECRET_KEY'] = config['SECRET_KEY']['KEY']
+jwt = JWTManager(app)
+
+
 
 @app.route('/sign_up', methods=['POST'])
 def sign_up():
     data = request.get_json(cache=False)
 
-    print(data)
-
     username = data['username']
     password = data['password']
     nickname = data['nickname']
     email = data['email']
+    auth = data['auth']
 
-    user = db.user.find_one({'username': username})
 
-    if user is not None:
-        return jsonify({'result': 'fail'}), 409
+    if not auth == session.get('auth_num'):
+        return jsonify({'result': 'fail'}), 400
     else:
-        user_info = {
-            'username': username,
-            'password': bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()),
-            'nickname': nickname,
-            'email': email
-        }
-        db.user.insert_one(user_info)
-        return jsonify({'result': 'success'}), 200
+        user = db.user.find_one({'username': username})
+
+        if user is not None:
+            return jsonify({'result': 'fail'}), 409
+        else:
+            user_info = {
+                'username': username,
+                'password': bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()),
+                'nickname': nickname,
+                'email': email
+            }
+            db.user.insert_one(user_info)
+            return jsonify({'result': 'success'}), 200
 
 
 @app.route('/login', methods=['POST'])
@@ -49,14 +55,14 @@ def login():
 
     username = data['username']
     password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
-
     user_check = db.user.find_one({'username': username}, {'password': password})
 
     if user_check is None:
-        return jsonify({'result': 'fail'}), 400
+        return jsonify({'result': 'fail'}), 401
     else:
-        return jsonify({'result': 'success'})
-    
+        access_token = create_access_token(identity=username)
+    return jsonify({'result': 'success', 'token': access_token}), 200
+
 
 @app.route('/authentication', methods=['POST'])
 def email_send():
@@ -81,19 +87,16 @@ def email_send():
     smtp.close()
     session['auth_num'] = random_number
     return jsonify({'result': 'success'}), 200
-
-
-@app.route('/verify', methods=['POST'])
-def verify():
-    data = request.get_json(cache=False)
-
-    if data['auth_num'] == session['auth_num']:
-        session.pop('auth_num')
-        return jsonify({'result': 'success'}), 200
-    else:
-        session.pop('auth_num')
-        return jsonify({'result': 'fail'}), 400
     
+@app.route('/logout', methods=['GET'])
+def logout():
+    return jsonify({'logout': 'success'}), 200
+
+
+@app.route('/tempWrite', methods=['POST'])
+def write():
+    data = request.get_json(cache=False)
+    print(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
