b75f39bba9fed3af87ec2557cb9cd6a5ac41ef2a
Yoengjung <rhdudwnd82@naver.com>
2023-06-13 14:59:29 +0900

    logout

--- frontend/public/js/logout.js
+++ frontend/public/js/logout.js
@@ -1,14 +1,25 @@
 require("dotenv").config();
 
-function logout() {
-  fetch(`/logout/${BACKEND_URL}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .catch((err) => {
-      console.error("로그아웃 중 에러 발생");
-    });
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const logoutBtn = document.getElementById("logout-button");
+
+  logoutBtn.addEventListener("click", function (event) {
+    fetch(`/logout${BACKEND_URL}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.success) {
+          location.href = "/login";
+        } else {
+          alert(res.msg);
+        }
+      })
+      .catch((err) => {
+        console.error("로그아웃 중 에러 발생");
+      });
+  });
+});
