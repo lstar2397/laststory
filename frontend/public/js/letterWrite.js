38314eaffd4a397f95720d587a9648dd7fea2a03
Yoengjung <rhdudwnd82@naver.com>
2023-06-13 09:59:24 +0900

    게시 modal

--- frontend/public/js/letterWrite.js
+++ frontend/public/js/letterWrite.js
@@ -2,13 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
   const btnOpenModal = document.querySelector(".btn-modal");
   const modal = document.querySelector(".modal");
   const closeModalBtn = document.querySelector(".close");
-  const publishBtn = document.getElementById("btn-yes");
-  const publicRadio = document.getElementById("public");
-  const privateRadio = document.getElementById("private");
-  const letterPublic = document.querySelector(".letter-Public");
   const letterPrivate = document.querySelector(".letter-Private");
 
   btnOpenModal.addEventListener("click", function () {
+    const title = document.getElementById("title").value;
+    const content = document.getElementById("content").value;
+
+    if (title === "") {
+      alert("제목을 입력해주세요.");
+      return;
+    }
+    if (content === "") {
+      alert("내용을 입력해주세요.");
+      return;
+    }
     modal.style.display = "block";
   });
 
@@ -22,17 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  publicRadio.addEventListener("change", function () {
-    letterPublic.style.display = "block";
-    letterPrivate.style.display = "none";
-  });
-
-  privateRadio.addEventListener("change", function () {
-    letterPublic.style.display = "none";
-    letterPrivate.style.display = "block";
-  });
-
-  publishBtn.addEventListener("click", function () {
+  addTarget.addEventListener("click", function () {
     const emailInput = document.createElement("input");
     emailInput.type = "email";
     emailInput.name = "publicTarget";
@@ -40,32 +37,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
     letterPrivate.appendChild(emailInput);
   });
-  });
-
-  // 초기에 공개 옵션 선택
-  publicRadio.checked = true;
-  letterPublic.style.display = "block";
-  letterPrivate.style.display = "none";
-
-  publishBtn.addEventListener("click", function () {
-    const title = document.getElementById("title").value;
-    const content = document.getElementById("content").value;
-
-    if (!publicRadio.checked) {
-      const publicDate = document.getElementById("publicDate").value;
-      const publicTime = document.getElementById("publicTime").value;
-      const publicDateTime = publicDate + " " + publicTime;
-      const publicDateObj = new Date(publicDateTime);
-      const now = new Date();
-
-      if (publicDateObj < now) {
-        alert("현재 시간 이후로 설정해주세요.");
-        return;
-      }
-      const publicTarget = document.getElementById("publicTarget").value;
-    }
-  });
 });
+
+function pushLetter(event) {
+  const title = document.getElementById("title").value;
+  const content = document.getElementById("content").value;
+
+  const publicDate = document.getElementById("publicDate").value;
+  const publicTime = document.getElementById("publicTime").value;
+  const publicDateTime = publicDate + " " + publicTime;
+  const publicDateObj = new Date(publicDateTime);
+  const now = new Date();
+
+  if (publicDateObj < now) {
+    alert("현재 시간 이후로 설정해주세요.");
+    return;
+  }
+
+  const publicTarget = document.getElementById("publicTarget").value;
+  console.log(title, content, publicDateTime, publicTarget);
+}
 
 function tempSave(event) {
   event.preventDefault();
