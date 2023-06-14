f7f5a7779d2af40f5b4fcf5cc8c07e8b8b77f6bb
dora2emong <44566304+dora2emong@users.noreply.github.com>
2023-06-14 15:44:09 +0900

    token 확인 추가

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,6 @@
-import bcrypt, smtplib, random, redis, base64, jwt
+import bcrypt, smtplib, random, jwt, json
 from datetime import datetime, timedelta
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from configparser import ConfigParser
 from email.message import EmailMessage
@@ -18,8 +18,26 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 # 비밀 키 설정
 app.secret_key = config['SECRET_KEY']['KEY']
-redis_client = redis.Redis(host='localhost', port=6379)
 
+
+def is_token_exist(token):
+    token = request.headers.get('Authorization')
+
+    if token:
+        # Bearer 접두사 제거
+        token = token.replace('Bearer ', '')
+
+        try:
+            # 토큰 디코딩
+            decoded_token = jwt.decode(token, 'secret_key', algorithms=['HS256'])
+            data = json.loads(decoded_token['data'])
+            return data
+        except jwt.ExpiredSignatureError:    # 토큰 만료 시 예외 처리
+            return None
+        except jwt.InvalidTokenError:    # 잘못된 토큰 예외 처리
+            return None
+    else:
+        return None
 
 @app.route('/sign_up', methods=['POST'])
 def sign_up():
@@ -53,20 +71,19 @@ def sign_up():
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json(cache=False)
-
-        username = data['username']
-        password = data['password']
-        user_check = db.user.find_one({'username': username})
-        if user_check is not None and bcrypt.checkpw(password.encode('utf-8'), user_check['password']):
-            payload = {
-                'username': username,
-                'exp': datetime.utcnow() + timedelta(minutes=30)
-            }
-            token = jwt.encode(payload, app.secret_key, algorithm='HS256')
-            return jsonify({'result': 'success', 'token': token}), 200
-        else:
-            # 로그인 실패
-            return jsonify({'result': 'fail'}), 401
+    username = data['username']
+    password = data['password']
+    user_check = db.user.find_one({'username': username})
+    if user_check is not None and bcrypt.checkpw(password.encode('utf-8'), user_check['password']):
+        payload = {
+            'username': username,
+            'exp': datetime.utcnow() + timedelta(minutes=30)
+        }
+        token = jwt.encode(payload, app.secret_key, algorithm='HS256')
+        return jsonify({'result': 'success', 'token': token}), 200
+    else:
+        # 로그인 실패
+        return jsonify({'result': 'fail'}), 401
 
 
 @app.route('/authentication', methods=['POST'])
@@ -109,11 +126,11 @@ def write():
     data = request.get_json(cache=False)
     title = data['title']
     encrypted = data['encrypted']
-    username = redis_client.get('username')
-    print(username)
-    if username is None:
+    token = is_token_exist()
+    if token is None:
         return jsonify({'result': 'fail'}), 400
     else:
+        username = token['username']
         tempPosts = {
             'username': username,
             'writing_time': datetime.datetime.utcnow(),
@@ -122,6 +139,8 @@ def write():
         }
         db.temp_post.insert_one(tempPosts)
         return jsonify({'result': 'success'}), 200
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
